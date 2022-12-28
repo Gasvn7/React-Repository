a87ecb7762b317373f5b510b283fce9b2afc2fbc
Gasvn7 <morenogaston595@gmail.com>
2022-12-28 17:24:13 -0300

    Navbar - Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
+/* import Button from "react-bootstrap/button"; */
+import Container from "react-bootstrap/container";
+import Nav from "react-bootstrap/nav";
+import Navbar from "react-bootstrap/navbar";
+import NavDropdown from "react-bootstrap/navDropdown";
+
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Navbar.Brand href="/">
+                  Amaji
+                </Navbar.Brand>
+            </Nav>
+            <Nav>
+                <Nav.Link href="/">Home</Nav.Link>
+                <NavDropdown title="Projects" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Petitos Petshop</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Pokemon React [Soon]
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="/">Contact me</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
